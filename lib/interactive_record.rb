84a3877b2069bca643d7383291d66862719ad3e9
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-06-28 23:17:14 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -33,7 +33,7 @@ class InteractiveRecord
      DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?",name )
    end
    def self.find_by(value)
-     value
+     p value
        DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?",name )
    end
 end
